82381228b6afeaa9385365070c597b9ce771e0e4
jbt <jbt@405_09>
2021-06-08 15:56:29 +0300

    Something

--- Java Course/src/learning/Main.java
+++ Java Course/src/learning/Main.java
@@ -10,22 +10,36 @@ public class Main {
 
 		Pearson[] persons = new Pearson[3];
 
-		// get input from the user, then call the to person ctor with the user values
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) { // Creating user names and passwords.
 			System.out.println("Choose a username");
 			String user = sc.nextLine();
 			System.out.println("Choose a password");
 			String pass = sc.nextLine();
 			persons[i] = new Pearson(user, pass);
 		}
-		
-		do {
+		boolean loggedIn = false;
+
+		do { // logging in.
+			loggedIn = false;
+
+			System.out.println("Enter index");
+			int index = sc.nextInt() - 1;
+			if (index < 0 || index > persons.length) {
+				System.out.println("invalid Index, try again: ");
+				continue;
+			}
+			System.out.println("Enter username");
 			String user = sc.nextLine();
+			if (user != persons[index].getUser()) {
+				System.out.println("wrong username, try again: ");
+				continue;
+			}
+			System.out.println("Enter passwword");
 			String pass = sc.nextLine();
-			
-			
-		}while()
-		
+			if (pass != persons[index].getPass()) {
+			}
+
+		} while (loggedIn == false);
 
 	}
 
