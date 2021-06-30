398e84c4020fc5a5463e51aaa6bf8a73c0ece816
Yossi <85559475+Exseibar@users.noreply.github.com>
2021-06-30 15:19:11 +0300

    Add files via upload

---
+++ API Exercise/src/api/Program.java
@@ -0,0 +1,103 @@
+package api;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Program {
+
+	private static Scanner sc = new Scanner(System.in);
+	private static int nextTaskId = 1;
+
+	public static Task getTaskFromUser() throws TaskException {
+		System.out.println("Enter task description");
+		String description = sc.nextLine();
+		description = sc.nextLine();
+		System.out.println("Enter deadline for the task: hh:mm:ss");
+		String time = sc.nextLine();
+		LocalDateTime deadline = null;
+		try {
+			deadline = LocalDateTime.of(LocalDate.now(), LocalTime.parse(time));
+			Task task = new Task(nextTaskId++, description, deadline);
+			return task;
+		} catch (DateTimeParseException e) {
+			throw new TaskException("Invalid time added. task not added");
+		}
+	}
+
+	public static void printMenu() {
+
+		System.out.println("===============Task Menu===============\n");
+		System.out.println("1.============Add new Task=============\n");
+		System.out.println("2.============Remove Task==============\n");
+		System.out.println("3.==========Set a Task to Done=========\n");
+		System.out.println("3.===========Show Task by ID===========\n");
+		System.out.println("4.===========Show all Tasks============\n");
+		System.out.println("5.========Show Tasks Due Until=========\n");
+		System.out.println("6.============Exit Program=============\n");
+
+	}
+
+	public static void main(String[] args) {
+		Scheduler scheduler = new Scheduler();
+		int sleepTime = 5000;
+		boolean exit = false;
+		int taskId;
+		scheduler.startMonitoringTasks();
+		while (!exit) {
+			printMenu();
+			int option = sc.nextInt();
+			switch (option) {
+
+			case 1:
+
+				try {
+					scheduler.addTask(getTaskFromUser());
+					System.out.println("The task was added successfully!");
+				} catch (TaskException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+
+			case 2:
+
+				System.out.println(scheduler.getAllTasks());
+				System.out.println("Enter task id to remove task.");
+				taskId = sc.nextInt();
+				try {
+					scheduler.removeTask(taskId);
+					System.out.println("Task removed successfully");
+				} catch (TaskException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+
+			case 3:
+				System.out.println("Enter a task id.");
+				taskId = sc.nextInt();
+				try {
+					scheduler.getTask(taskId).doTask();
+					System.out.println("Task " + scheduler.getTask(taskId).getId() + " has been set to done");
+				} catch (TaskException e) {
+					System.out.println(e.getMessage());
+				}
+			case 4:
+
+				scheduler.displayTasks();
+				try {
+					Thread.sleep(sleepTime);
+				} catch (InterruptedException e) {
+					System.out.println("Do not interrupt");
+				}
+				break;
+
+			case 5:
+				System.out.println("enter a hh:mm:ss");
+
+			}
+		}
+
+	}
+}
--- API Exercise/src/api/Scheduler.java
+++ API Exercise/src/api/Scheduler.java
@@ -1,16 +1,16 @@
 package api;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Scheduler extends Thread {
+public class Scheduler {
 
-	List<Task> tasks;
+	private List<Task> tasks;
 	private boolean monitoringActive;
-	private long checkRateMillis;
+	private long checkRateMillis = 5000;
 
 	public Scheduler() {
-
 		tasks = new ArrayList<Task>();
 	}
 
@@ -18,8 +18,12 @@ public class Scheduler extends Thread {
 		this.tasks.add(task);
 	}
 
-	public void removeTask(int index) {
-		this.tasks.remove(index);
+	public void removeTask(int id) throws TaskException {
+		try {
+			this.tasks.remove(id - 1);
+		} catch (IndexOutOfBoundsException e) {
+			throw new TaskException("Task does not exist.");
+		}
 	}
 
 	public void displayTasks() {
@@ -29,12 +33,69 @@ public class Scheduler extends Thread {
 		System.out.println(tasks);
 	}
 
-	public Task getTask(int index) {
-		return tasks.get(index);
+	public Task getTask(int id) throws TaskException {
+		try {
+			return tasks.get(id - 1);
+		} catch (IndexOutOfBoundsException e) {
+			throw new TaskException("Task does not exist.");
+		}
 	}
 
 	public List<Task> getAllTasks() {
 		return tasks;
+	}
+
+	public List<Task> getAllTasksDueUntil(LocalDateTime deadline) {
+		List<Task> listDueUntil = new ArrayList<Task>();
+		for (Task task : tasks) {
+			if (task.getDeadline().isBefore(deadline)) {
+				listDueUntil.add(task);
+			}
+		}
+		return listDueUntil;
+
+	}
+
+	public void startMonitoringTasks() {
+		SchedulerThread schedulerThread = new SchedulerThread();
+		schedulerThread.start();
+		monitoringActive = true;
+	}
+
+	public void stopMonitoringTasks() {
+		monitoringActive = false;
+	}
+
+	public void checkDeadlines() {
+		for (Task t : tasks) {
+			if (t.isAlertPopped()) {
+				continue;
+			}
+			if (t.getDeadline().isBefore(LocalDateTime.now()) && !t.isAlertPopped()) {
+				System.out.println("DEADLINE ALERT: " + t);
+			}
+			t.setAlertPopped(true);
+		}
+	}
+
+	public class SchedulerThread extends Thread {
+
+		public SchedulerThread() {
+
+			this.setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					sleep(checkRateMillis);
+				} catch (InterruptedException e) {
+					System.out.println("Do not interrupt!");
+				}
+				checkDeadlines();
+			}
+		}
 	}
 
 }
