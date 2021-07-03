295278014537a81515d779d2e5f7bb6eaa373f70
Yossi <85559475+Exseibar@users.noreply.github.com>
2021-07-03 18:25:57 +0300

    Add files via upload

---
+++ Coupons Project/src/javaBeans/Category.java
@@ -0,0 +1,18 @@
+package javaBeans;
+
+/**
+ *	This class represents the categories for the coupons.
+ * 
+ * @author Yossi and Avi Toohband
+ *
+ */
+public enum Category {
+	
+	FOOD,
+	
+	ELECTRICITY,
+	
+	RESTAURANT,
+	
+	VACATION,
+}
